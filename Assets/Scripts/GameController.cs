48763618ffa8e7be38ff7ddea1e4ab9048147210
Hung Nguyen <vahu.ngx@gmail.com>
2023-05-10 22:54:55 +0700

    demo

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -58,6 +58,7 @@ public class GameController : MonoBehaviour
         pnlEndGame.SetActive(true);
         txtEndGame.text = "Your Point: " + gamePoint.ToString();
         isStartGame = false;
+        txtPoint.text = "";
     }
     public void GetPoint()
     {
